8ea78da354767e8f14813fe7e2b84519d3bfc95f
sivolc <clovisvt@gmail.com>
2023-12-02 11:50:21 -0800

    working test run with autogpt stable branch

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
-import instructor
-from openai import OpenAI
-from pydantic import BaseModel
+import yaml
 
-# Apply the patch to the OpenAI client
-client = instructor.patch(OpenAI())
+# Load the YAML file
+with open('autorepos/AutoGPT/ai_settings.yaml', 'r') as file:
+    data = yaml.safe_load(file)
 
-# Define the Pydantic Model
-class UserDetails(BaseModel):
-    name: str
-    age: int
+# Extract the fields
+ai_name = data['ai_name']
+api_budget = data['api_budget']
 
-# Extract data
-user: UserDetails = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    response_model=UserDetails,
-    messages=[
-        {"role": "user", "content": "Extract Jason is 25 years old"},
-    ]
-)
+# Create custom variables
+ai_purpose = """an AI assistant specialized in doing research on a topic and building a markdown file .md"""
+topic = """Cat videos"""
+context = """The user is a young adult, with a passion for understanding the world mathematically"""
 
-# Assertions
-assert user.name == "Jason"
-assert user.age == 25
+# Function to write back to the YAML file
+def write_to_yaml(data, filename='autorepos/AutoGPT/ai_settings.yaml'):
+    with open(filename, 'w') as file:
+        yaml.safe_dump(data, file)
+
+# Update the data
+# data['ai_purpose'] = ai_purpose
+# data['context'] = context
+ai_role = ai_purpose + context
+data['ai_role'] = ai_purpose + context
+
+
+data['ai_goals'] = [
+    "Utilize web search capabilities to gather a summary of information on the {} as it relates to {}.".format(topic, context),
+    "Filter and curate into a single markdown file",
+    "Add a summary and tags using '#'"
+]
+
+# Write back to the YAML file
+write_to_yaml(data)
+
+import subprocess
+
+subprocess.call(["bash", "run_continuous.sh", '-y'], cwd="/home/lightsong/Central/Projects/hackathons/b3vault/autorepos/AutoGPT")
